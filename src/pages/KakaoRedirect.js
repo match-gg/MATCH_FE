3579d15fa79d44d9f24ef2dc2e582a2b9dd0b5fb
Jooyeob Na <zooby88@naver.com>
2023-02-17 13:28:10 +0900

    feat : 카카오 토큰 발급 및 전달

--- src/pages/KakaoRedirect.js
+++ src/pages/KakaoRedirect.js
@@ -1,31 +1,49 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
+import { tokenActions } from '../store/token-slice';
+import { userActions } from '../store/user-slice';
+
 import { CircularProgress, Box, Typography } from '@mui/material';
 
 import SimpleLayout from '../components/ui/SimpeLayout';
-
-const SERVER_URL = '';
 
 const KakaoRedirect = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const kakaoLogin = () => {
+    const kakaoLogin = async () => {
       // 1. 페이지 URL에서 인가코드 받아오기
       let params = new URL(document.URL).searchParams;
       let code = params.get('code');
 
-      // 2. server 로 인가코드 전달 ( queryString )
-      axios.get(`${SERVER_URL}/api/auth?code=${code}`).then((response) => {
-        console.log(response);
 
-        // refreshToken -> cookie with HTTP Only and Secure
-        // accessToken -> Redux Store
-      });
+      // 2. 인가코드로 카카오 액세스 토큰 발급받기
+      try {
+        const {
+          data: { access_token: kakaoAccessToken },
+        } = await axios('https://kauth.kakao.com/oauth/token', {
+          params: {
+            grant_type: 'authorization_code',
+            client_id: process.env.REACT_APP_REST_API_KEY,
+            redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+            code: code,
+          },
+        });
+
+        // 3. 토큰을 서버로 전송해 가입 여부 확인하기
+        const response = await axios.post(`${process.env}/api/user/signup`, {
+          oauth2AccessToken : kakaoAccessToken,
+        })
+
+        // 4. reponse 확인해서 로그인 처리 / 회원가입 플로우로 이동
+
+      } catch (e) {
+        console.log(e);
+      }
     };
 
     kakaoLogin();
