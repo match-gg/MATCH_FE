cf0a5f1f0c68ef7d460e79161338e62d72716819
zooby <zooby88@naver.com>
2023-02-27 14:03:31 +0900

    fix : kakaoLogin 오타 수정

--- src/pages/KakaoRedirect.js
+++ src/pages/KakaoRedirect.js
@@ -3,7 +3,6 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import { tokenActions } from '../store/token-slice';
-import { userActions } from '../store/user-slice';
 import { api } from '../api/api';
 
 import { CircularProgress, Box, Typography } from '@mui/material';
@@ -18,41 +17,39 @@ const KakaoRedirect = () => {
 
   useEffect(() => {
     const kakaoLogin = async () => {
-      const kakaoLogin = async () => {
-        // 1. 페이지 URL에서 인가코드 받아오기
-        let params = new URL(document.URL).searchParams;
-        let code = params.get('code');
+      // 1. 페이지 URL에서 인가코드 받아오기
+      let params = new URL(document.URL).searchParams;
+      let code = params.get('code');
 
-        // 2. 토큰을 서버로 전송해 가입 여부 확인하기
-        const response = await api
-          .post(`http://localhost:8080/api/user/signup`, {
-            kakaoAuthCode: code,
-          })
-          .catch(async (error) => {
-            if (error.response.status === 400 && error.response.data['status']) {
-              const response = await api.post(`http://localhost:8080/api/user/signin`, {
-                kakaoAuthCode: code,
-              });
+      // 2. 토큰을 서버로 전송해 가입 여부 확인하기
+      const response = await api
+        .post(`http://localhost:8080/api/user/signup`, {
+          kakaoAuthCode: code,
+        })
+        .catch(async (error) => {
+          if (error.response.status === 400 && error.response.data['status']) {
+            const response = await api.post(`http://localhost:8080/api/user/signin`, {
+              kakaoAuthCode: code,
+            });
 
-              const { accessToken, refreshToken } = response.data['jwtToken'];
+            const { accessToken, refreshToken } = response.data['jwtToken'];
 
-              dispatch(tokenActions.SET_TOKEN(accessToken));
-              localStorage.setItem('matchGG_refreshToken', refreshToken);
-              navigate('/leagueoflegends');
-            } else {
-              alert(`비정상적인 접근입니다.`);
-            }
-          });
+            dispatch(tokenActions.SET_TOKEN(accessToken));
+            localStorage.setItem('matchGG_refreshToken', refreshToken);
+            navigate('/leagueoflegends');
+          } else {
+            alert(`비정상적인 접근입니다.`);
+          }
+        });
 
-        const { accessToken, refreshToken } = response.data['jwtToken'];
+      const { accessToken, refreshToken } = response.data['jwtToken'];
 
-        dispatch(tokenActions.SET_TOKEN(accessToken));
-        localStorage.setItem('matchGG_refreshToken', refreshToken);
+      dispatch(tokenActions.SET_TOKEN(accessToken));
+      localStorage.setItem('matchGG_refreshToken', refreshToken);
 
-        navigate('/register');
-      };
+      navigate('/register');
     };
-    
+
     kakaoLogin();
   }, [dispatch, navigate]);
 
