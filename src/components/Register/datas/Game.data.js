0b1b171cad4e1636ff319cfde6e36a3ae03d7f23
zooby <zooby88@naver.com>
2023-03-17 22:00:41 +0900

    docs : 게임 정보 수정

--- src/components/Register/datas/Game.data.js
+++ src/components/Register/datas/Game.data.js
@@ -8,31 +8,31 @@ export const GameDatas = [
   {
     gameIcon: lolIcon,
     altMessage: '리그오브레전드_아이콘',
-    labelText: '소환사명을 입력하세요',
+    labelText: '리그오브레전드 소환사명을 입력하세요',
     gameName: 'lol',
   },
   {
     gameIcon: pubgIcon,
     altMessage: '배틀그라운드_아이콘',
-    labelText: '캐릭터명을 입력하세요.',
+    labelText: '배틀그라운드 캐릭터명을 입력하세요.',
     gameName: 'pubg',
   },
   {
     gameIcon: overwatchIcon,
     altMessage: '오버워치_아이콘',
-    labelText: '플레이어 이름 또는 배틀태그#1234 입력하세요.',
+    labelText: '오버워치 플레이어 이름 또는 배틀태그#1234 입력하세요.',
     gameName: 'overwatch',
   },
   {
     gameIcon: lostarkIcon,
     altMessage: '로스트아크_아이콘',
-    labelText: '캐릭터명을 입력하세요',
+    labelText: '로스트아크 캐릭터명을 입력하세요',
     gameName: 'lostark',
   },
   {
     gameIcon: maplestoryIcon,
     altMessage: '메이플스토리_아이콘',
-    labelText: '캐릭터명을 입력하세요',
+    labelText: '메이플스토리 대표 캐릭터명을 입력하세요',
     gameName: 'maplestory',
   },
 ];
