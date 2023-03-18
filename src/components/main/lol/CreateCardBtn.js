5bb47483c1d3cd61dec46d00ce0ae46c545d2dba
Minggurazi <jhsong1450@naver.com>
2023-03-19 00:03:03 +0900

    fix : Modal UI 수정

--- src/components/main/lol/CreateCardBtn.js
+++ src/components/main/lol/CreateCardBtn.js
@@ -22,6 +22,7 @@ import BackspaceIcon from '@mui/icons-material/Backspace';
 import { Fragment, useEffect, useState } from 'react';
 import MicIcon from '@mui/icons-material/Mic';
 import { api } from '../../../api/api';
+import CloseIcon from '@mui/icons-material/Close';
 
 const ModalText = ({ text, type }) => {
   return (
@@ -57,9 +58,16 @@ const CreateCardBtn = (props) => {
     if (newValue === null) {
       return;
     } else if (newValue === 'ARAM') {
-      setUserInput({ ...userInput, position: [], type: newValue });
+      setUserInput({ ...userInput, position: [], tier: [], type: newValue });
     } else {
       if (userInput.type !== 'DUO_RANK' && newValue === 'DUO_RANK') {
+        setUserInput({
+          ...userInput,
+          tier: ['IRON'],
+          position: ['TOP'],
+          type: newValue,
+        });
+      } else if (userInput.type === 'ARAM' && newValue !== 'ARAM') {
         setUserInput({
           ...userInput,
           tier: ['IRON'],
@@ -151,6 +159,7 @@ const CreateCardBtn = (props) => {
   };
   const postModalInfo = async () => {
     console.log(userInput);
+    //서버로 전송
     await api.post(`/api/lol/board`, { ...userInput }).catch((error) => {
       alert('게시글 작성중 문제가 발생하였습니다.\n다시 시도해주세요.');
       console.log(error);
@@ -169,38 +178,42 @@ const CreateCardBtn = (props) => {
         BackdropProps={{ onClick: handleBackdropClick }}
       >
         {/* hideBackdrop 속성을 사용하면 배경 클릭해도 안닫히기는 하는데 배경이 blur 처리되는것도 동작하지 않음 (그냥 배경을 안만들어주는듯) */}
-        <Grid
-          container
+        <Stack
+          direction='column'
+          justifyContent='center'
+          alignItems='stretch'
           sx={{
             top: '50%',
             left: '50%',
             position: 'absolute',
             transform: 'translate(-50%, -50%)',
-            width: '80vw',
+            width: '55vw',
             // height: '70%',
             bgcolor: 'white',
-            padding: '20px 50px 15px 50px',
+            padding: '10px 30px 15px 30px',
             // minHeight: '70vw',
             // minWidth: '1000px',
             // maxHeight: '650px',
-            maxWidth: '1200px',
-            boxShadow: '5px 10px 10px 1px rgba(0,0,0,.3)',
+            maxWidth: '700px',
+            // boxShadow: '5px 10px 10px 1px rgba(0,0,0,.3)',
+            border: '1px solid #dddddd',
           }}
         >
-          <Grid
-            item
-            xs={6}
+          {/* 아이디 */}
+          <Box
             sx={{
-              height: '80%',
-              paddingRight: '5px',
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
             }}
           >
-            {/* 아이디 */}
             <Box
               sx={{
                 display: 'flex',
                 flexDirection: 'row',
                 justifyContent: 'start',
+                alignItems: 'center',
               }}
             >
               <Switch
@@ -210,8 +223,6 @@ const CreateCardBtn = (props) => {
               <Typography
                 color={idConnected ? 'primary' : 'grey'}
                 sx={{
-                  display: 'flex',
-                  alignItems: 'center',
                   fontSize: '16px',
                   fontWeight: 'bold',
                 }}
@@ -220,183 +231,176 @@ const CreateCardBtn = (props) => {
               </Typography>
             </Box>
             <Box
-              sx={{ maxWidth: '350px', display: 'felx', flexDirection: 'row' }}
-            >
-              <OutlinedInput
-                fullWidth
-                placeholder='연결할 아이디를 입력해주세요.'
-                disabled={idConnected}
-                onChange={handleName}
-                endAdornment={
-                  <Button
-                    position='end'
-                    sx={{ whiteSpace: 'nowrap' }}
-                    onClick={certifyNickname}
-                    disabled={idConnected}
-                  >
-                    인증하기
-                  </Button>
-                }
-              />
-            </Box>
-            {/* 큐타입 */}
-            <ModalText text={'플레이할 큐타입'} />
-            <Box sx={{ overflow: 'auto ' }}>
-              <ToggleButtonGroup
-                value={userInput.type}
-                exclusive={true}
-                onChange={handleType}
-                sx={{
-                  '& .MuiToggleButton-root.Mui-selected': {
-                    backgroundColor: '#4f90db',
-                    color: 'white',
-                  },
-                }}
-              >
-                <ToggleButton value='DUO_RANK' sx={{ whiteSpace: 'nowrap' }}>
-                  듀오 랭크
-                </ToggleButton>
-                <ToggleButton value='FREE_RANK' sx={{ whiteSpace: 'nowrap' }}>
-                  자유 랭크
-                </ToggleButton>
-                <ToggleButton value='NORMAL' sx={{ whiteSpace: 'nowrap' }}>
-                  노말 게임
-                </ToggleButton>
-                <ToggleButton value='ARAM' sx={{ whiteSpace: 'nowrap' }}>
-                  무작위 총력전
-                </ToggleButton>
-              </ToggleButtonGroup>
-            </Box>
-            {/* 티어 */}
-            <ModalText text={'원하는 파티원의 티어'} />
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <ToggleButtonGroup
-                value={userInput.tier}
-                onChange={handleTier}
-                exclusive={userInput.type === 'DUO_RANK' ? true : false}
-                sx={{
-                  '& .MuiToggleButton-root.Mui-selected': {
-                    backgroundColor: '#4f90db',
-                    color: 'white',
-                  },
-                }}
-              >
-                <ToggleButton value='IRON'>IRON</ToggleButton>
-                <ToggleButton value='BRONZE'>BRONZE</ToggleButton>
-                <ToggleButton value='SILVER'>SILVER</ToggleButton>
-                <ToggleButton value='GOLD'>GOLD</ToggleButton>
-              </ToggleButtonGroup>
-              <ToggleButtonGroup
-                value={userInput.tier}
-                onChange={handleTier}
-                exclusive={userInput.type === 'DUO_RANK' ? true : false}
-                sx={{
-                  '& .MuiToggleButton-root.Mui-selected': {
-                    backgroundColor: '#4f90db',
-                    color: 'white',
-                  },
-                }}
-              >
-                <ToggleButton value='PLATINUM'>PLATINUM</ToggleButton>
-                <ToggleButton value='DIAMOND'>DIAMOND</ToggleButton>
-                <ToggleButton value='MASTER'>MASTER</ToggleButton>
-              </ToggleButtonGroup>
-            </Box>
-            {/* 포지션 */}
-            <ModalText text={'원하는 파티원의 포지션'} type={userInput.type} />
-            <ToggleButtonGroup
-              disabled={userInput.type === 'ARAM' ? true : false}
-              value={userInput.position}
-              exclusive={userInput.type === 'DUO_RANK' ? true : false}
-              onChange={handlePosition}
               sx={{
-                '& .MuiToggleButton-root.Mui-selected': {
-                  backgroundColor: '#4f90db',
-                  color: 'white',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                '&:hover': {
+                  cursor: 'pointer',
+                  boxShadow: '0 0 2px 1px #4f8fdb',
                 },
               }}
             >
-              <ToggleButton value='TOP'>TOP</ToggleButton>
-              <ToggleButton value='JUG'>JUG</ToggleButton>
-              <ToggleButton value='MID'>MID</ToggleButton>
-              <ToggleButton value='ADC'>ADC</ToggleButton>
-              <ToggleButton value='SUP'>SUP</ToggleButton>
-            </ToggleButtonGroup>
-            {/* 카드 만료 시간 */}
-            <ModalText text={'카드 만료 시간'} />
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              {/* 만료 시간이 4개인 경우  */}
-              {/* <ToggleButtonGroup
-                sx={{
-                  '& .MuiToggleButton-root.Mui-selected': {
-                    backgroundColor: '#4f90db',
-                    color: 'white',
-                  },
-                }}
-                value={userInput.expire}
-                exclusive
-                onChange={handleExpire}
-              >
-                <ToggleButton value='FIFTEEN_M'>15Min</ToggleButton>
-                <ToggleButton value='THIRTY_M'>30Min</ToggleButton>
-                <ToggleButton value='ONE_H'>1Hour</ToggleButton>
-                <ToggleButton value='TWO_H'>2Hour</ToggleButton>
-              </ToggleButtonGroup> */}
-              <ToggleButtonGroup
-                sx={{
-                  '& .MuiToggleButton-root.Mui-selected': {
-                    backgroundColor: '#4f90db',
-                    color: 'white',
-                  },
-                }}
-                value={userInput.expire}
-                exclusive
-                onChange={handleExpire}
-              >
-                <ToggleButton value='THREE_H'>3Hour</ToggleButton>
-                <ToggleButton value='SIX_H'>6Hour</ToggleButton>
-                <ToggleButton value='TWELVE_H'>12Hour</ToggleButton>
-                <ToggleButton value='TWENTY_FOUR_H'>24Hour</ToggleButton>
-              </ToggleButtonGroup>
+              <CloseIcon color='primary' onClick={closeModalConfirm} />
             </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'start',
-              }}
-            >
-              <Box sx={{ display: 'flex', marginTop: '16px' }}>
-                <Switch defaultChecked={false} onChange={handleVoice} />
-                <Typography
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    fontSize: '16px',
-                    fontWeight: 'bold',
-                  }}
+          </Box>
+          <Box
+            sx={{ maxWidth: '350px', display: 'felx', flexDirection: 'row' }}
+          >
+            <OutlinedInput
+              size='small'
+              fullWidth
+              placeholder='연결할 아이디를 입력해주세요.'
+              disabled={idConnected}
+              onChange={handleName}
+              endAdornment={
+                <Button
+                  position='end'
+                  sx={{ whiteSpace: 'nowrap' }}
+                  onClick={certifyNickname}
+                  disabled={idConnected}
                 >
-                  <MicIcon color={userInput.voice === 'Y' ? 'primary' : ''} />
-                  음성채팅 사용 여부
-                </Typography>
-              </Box>
+                  인증하기
+                </Button>
+              }
+            />
+          </Box>
+          {/* 큐타입 */}
+          <ModalText text={'플레이할 큐타입'} />
+          <ToggleButtonGroup
+            size='small'
+            value={userInput.type}
+            exclusive={true}
+            onChange={handleType}
+            sx={{
+              '& .MuiToggleButton-root.Mui-selected': {
+                backgroundColor: '#4f90db',
+                color: 'white',
+              },
+            }}
+          >
+            <ToggleButton value='DUO_RANK' sx={{ whiteSpace: 'nowrap' }}>
+              듀오 랭크
+            </ToggleButton>
+            <ToggleButton value='FREE_RANK' sx={{ whiteSpace: 'nowrap' }}>
+              자유 랭크
+            </ToggleButton>
+            <ToggleButton value='NORMAL' sx={{ whiteSpace: 'nowrap' }}>
+              노말 게임
+            </ToggleButton>
+            <ToggleButton value='ARAM' sx={{ whiteSpace: 'nowrap' }}>
+              무작위 총력전
+            </ToggleButton>
+          </ToggleButtonGroup>
+          {/* 티어 */}
+          <ModalText text={'원하는 파티원의 티어'} />
+          <ToggleButtonGroup
+            disabled={userInput.type === 'ARAM' ? true : false}
+            size='small'
+            value={userInput.tier}
+            onChange={handleTier}
+            exclusive={userInput.type === 'DUO_RANK' ? true : false}
+            sx={{
+              '& .MuiToggleButton-root.Mui-selected': {
+                backgroundColor: '#4f90db',
+                color: 'white',
+              },
+            }}
+          >
+            <ToggleButton value='IRON'>IRON</ToggleButton>
+            <ToggleButton value='BRONZE'>BRONZE</ToggleButton>
+            <ToggleButton value='SILVER'>SILVER</ToggleButton>
+            <ToggleButton value='GOLD'>GOLD</ToggleButton>
+            <ToggleButton value='PLATINUM'>PLATINUM</ToggleButton>
+            <ToggleButton value='DIAMOND'>DIAMOND</ToggleButton>
+            <ToggleButton
+              value='MASTER'
+              disabled={userInput.type === 'DUO_RANK' ? true : false}
+            >
+              MASTER
+            </ToggleButton>
+            <ToggleButton
+              value='NONE'
+              disabled={userInput.type === 'DUO_RANK' ? true : false}
+            >
+              상관없음
+            </ToggleButton>
+          </ToggleButtonGroup>
+          {/* 포지션 */}
+          <ModalText text={'원하는 파티원의 포지션'} type={userInput.type} />
+          <ToggleButtonGroup
+            size='small'
+            disabled={userInput.type === 'ARAM' ? true : false}
+            value={userInput.position}
+            exclusive={userInput.type === 'DUO_RANK' ? true : false}
+            onChange={handlePosition}
+            sx={{
+              '& .MuiToggleButton-root.Mui-selected': {
+                backgroundColor: '#4f90db',
+                color: 'white',
+              },
+            }}
+          >
+            <ToggleButton value='TOP'>TOP</ToggleButton>
+            <ToggleButton value='JUG'>JUG</ToggleButton>
+            <ToggleButton value='MID'>MID</ToggleButton>
+            <ToggleButton value='ADC'>ADC</ToggleButton>
+            <ToggleButton value='SUP'>SUP</ToggleButton>
+            <ToggleButton value='NONE'>상관없음</ToggleButton>
+          </ToggleButtonGroup>
+          {/* 카드 만료 시간 */}
+          <ModalText text={'카드 만료 시간'} />
+          <Box>
+            <FormControl size='small' sx={{ width: '300px' }}>
+              <Select value={userInput.expire} onChange={handleExpire}>
+                <MenuItem value='FIFTEEN_M'>15분</MenuItem>
+                <MenuItem value='THIRTY_M'>30분</MenuItem>
+                <MenuItem value='ONE_H'>1시간</MenuItem>
+                <MenuItem value='TWO_H'>2시간</MenuItem>
+                <MenuItem value='THREE_H'>3시간</MenuItem>
+                <MenuItem value='SIX_H'>6시간</MenuItem>
+                <MenuItem value='TWELVE_H'>12시간</MenuItem>
+                <MenuItem value='TWENTY_FOUR_H'>24시간</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'start',
+            }}
+          >
+            <Box sx={{ display: 'flex', marginTop: '10px' }}>
+              <Switch defaultChecked={false} onChange={handleVoice} />
+              <Typography
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  fontSize: '16px',
+                  fontWeight: 'bold',
+                }}
+              >
+                <MicIcon color={userInput.voice === 'Y' ? 'primary' : ''} />
+                음성채팅 사용 여부
+              </Typography>
             </Box>
-          </Grid>
-          <Grid item xs={6} sx={{ height: '80%' }}>
+          </Box>
+          <Box xs={6} sx={{ height: '80%' }}>
             <ModalText text={'카드 내용 작성하기'} />
             <TextField
               sx={{ bgcolor: 'white' }}
               onChange={handleContent}
               fullWidth
               multiline
-              minRows={20} // 이거 어떻게 처리하지...
-              maxRows={20}
+              minRows={5} // 이거 어떻게 처리하지...
+              maxRows={5}
               placeholder='카드의 내용은 140자 이내로 작성해주세요.'
               inputProps={{ maxLength: 140 }}
             />
-          </Grid>
-          <Grid
-            item
+          </Box>
+          <Box
             xs={12}
             display='flex'
             justifyContent='space-between'
@@ -433,8 +437,8 @@ const CreateCardBtn = (props) => {
                 작성하기
               </Button>
             </Stack>
-          </Grid>
-        </Grid>
+          </Box>
+        </Stack>
       </Modal>
     </Fragment>
   );
