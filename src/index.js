3288ab9e1f7c40bf4986244ec305ea5d246e219f
zooby <zooby88@naver.com>
2023-03-23 16:21:14 +0900

    refactor : index.js 패키지 순서 정리

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { CookiesProvider } from 'react-cookie';
 
 import { CssBaseline } from '@mui/material';
 
 import App from './App';
 import store from './store/index';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
 
 const persistor = persistStore(store);
 
