be13dd4bb6251c3cafbf28fc8f901df3d7384e16
zooby <zooby88@naver.com>
2023-05-25 01:07:22 +0900

    fix : 카드상세모달 데이터 추가

---
+++ src/components/main/lol/CardDeatilModal.d.js
@@ -0,0 +1,37 @@
+export const position = [
+  { id: 'TOP', image: 'https://d18ghgbbpc0qi2.cloudfront.net/lol/lane_icons/TOP.png', kor: '탑' },
+  { id: 'JUG', image: 'https://d18ghgbbpc0qi2.cloudfront.net/lol/lane_icons/JUG.png', kor: '정글' },
+  { id: 'MID', image: 'https://d18ghgbbpc0qi2.cloudfront.net/lol/lane_icons/MID.png', kor: '미드' },
+  { id: 'ADC', image: 'https://d18ghgbbpc0qi2.cloudfront.net/lol/lane_icons/ADC.png', kor: '원딜' },
+  {
+    id: 'SPT',
+    image: 'https://d18ghgbbpc0qi2.cloudfront.net/lol/lane_icons/SUP.png',
+    kor: '서포터',
+  },
+  {
+    id: 'ALL',
+    image: 'https://d18ghgbbpc0qi2.cloudfront.net/lol/lane_icons/ALL1.png',
+    kor: '상관없음',
+  },
+];
+
+export const tierInfo = [
+  { id: 'IRON', color: '#51484A', kor: '아이언' },
+  { id: 'BRONZE', color: '#8C513A', kor: '브론즈' },
+  { id: 'SILVER', color: '#80989D', kor: '실버' },
+  { id: 'GOLD', color: '#CD8837', kor: '골드' },
+  { id: 'PLATINUM', color: '#4E9996', kor: '플래티넘' },
+  { id: 'DIAMOND', color: '#576BCE', kor: '다이아몬드' },
+  { id: 'MASTER', color: '#9A4EB0', kor: '마스터' },
+  { id: 'GRANDMASTER', color: '#CD4545', kor: '그랜드마스터' },
+  { id: 'CHALLENGER', color: '#F4C875', kor: '챌린저' },
+  { id: 'ALL', color: '#000000', kor: '모든티어' },
+];
+
+export const typeInfo = [
+  { id: 'DUO_RANK', kor: '솔로랭크', maxMember: 2 },
+  { id: 'FREE_RANK', kor: '자유랭크', maxMember: 5 },
+  { id: 'NORMAL', kor: '일반게임', maxMember: 5 },
+  { id: 'ARAM', kor: '칼바람나락', maxMember: 5 },
+  { id: 'ALL', kor: '모든큐', maxMember: 5 },
+];
