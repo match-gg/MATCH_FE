b482b4f9a3a8d69ab60fc26c1ee7a78e7167178d
zooby <zooby88@naver.com>
2023-05-25 00:23:21 +0900

    fix : 임시 게시글 추가

---
+++ src/components/main/pubg/boards.tmp.js
@@ -0,0 +1,56 @@
+export const temp_boards = [
+  {
+    id: 2,
+    oauth2Id: 'kakao2668291200',
+    name: 'Dsquad2',
+    // 구하고자 하는 사람의 정보 하위 3개
+    platform: 'STEAM',
+    type: 'RANK_SQUAD',
+    tier: 'GOLD',
+    // ---
+    voice: 'Y',
+    content: '같이 경쟁 스쿼드 돌릴 사람 구합니다. 마이크 가능하신 분이면 좋겠습니다.',
+    expire: 'FIFTEEN_M',
+    created: '2023-05-24 19:20:45',
+    author: {
+      name: 'Dsquad2', // 위의 name 과 동일
+      ratingPoint: 2239,
+      kill_summary: 93, // 전체 킬
+      death_summary: 35, // 전체 데스
+      // 위 두개로 킬퍼데스 ( pubg 에서 계산해서 던져주면 그냥 줘도 됨)
+      avgDmgDealt: 418.45, // 매치당 평균 딜량 ( 1킬이 100 데미지)
+      totalPlayed: 121, // 이번시즌 판수
+      totalWin: 12, // 치킨 횟수
+      totalTop10: 30, // 탑10 횟수
+    },
+    chatRoomId: '1234',
+    memberList: ['Dsquad2'],
+  },
+  {
+    id: 3,
+    oauth2Id: 'kakao2668291200',
+    name: 'PMTHK_',
+    // 구하고자 하는 사람의 정보 하위 3개
+    platform: 'KAKAO',
+    type: 'NORMAL_DUO',
+    tier: 'PLATINUM',
+    // ---
+    voice: 'N',
+    content: '같이 경쟁 스쿼드 돌릴 사람 구합니다. 마이크 가능하신 분이면 좋겠습니다.',
+    expire: 'SIX_H',
+    created: '2023-05-24 19:20:45',
+    author: {
+      name: 'Dsquad2', // 위의 name 과 동일
+      ratingPoint: 4232,
+      kill_summary: 120, // 전체 킬
+      death_summary: 35, // 전체 데스
+      // 위 두개로 킬퍼데스 ( pubg 에서 계산해서 던져주면 그냥 줘도 됨)
+      avgDmgDealt: 258.45, // 매치당 평균 딜량 ( 1킬이 100 데미지)
+      totalPlayed: 130, // 이번시즌 판수
+      totalWin: 5, // 치킨 횟수
+      totalTop10: 10, // 탑10 횟수
+    },
+    chatRoomId: '1234',
+    memberList: ['Dsquad2'],
+  },
+];
