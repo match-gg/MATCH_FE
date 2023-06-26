d7309bdf1b4af0db44580244996a8b45aad35981
Minggurazi <jhsong1450@naver.com>
2023-06-26 20:18:22 +0900

    feat : 우측 하단 바로가기 메뉴에서 notification 디스플레이 UI 디자인

--- src/shortcut/JoinedPartyCard.js
+++ src/shortcut/JoinedPartyCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-import { Box, Button, Stack, Typography } from '@mui/material';
+import { Badge, Box, Button, Stack, Typography } from '@mui/material';
 
 import lolImg from '../components/Register/logo_images/LoL_Icon_Flat_BLACK.png';
 import pubgImg from '../components/Register/logo_images/Pubg_Logo.png';
@@ -23,112 +23,113 @@ const JoinedPartyCard = (props) => {
   const location = useLocation();
 
   return (
-    // 테두리
-    <Box
-      sx={{
-        marginBottom: '20px',
-        border: '1px solid rgba(60, 57, 57, 0.5)',
-        borderRadius: '4px',
-        width: '300px',
-        minHeight: '110px',
-        backgroundColor: 'white',
-        padding: '4px',
-      }}
-    >
-      {/* 내용 */}
-      <Stack
-        spacing={1}
+    // badgeContent는 각 파티별로 뿌려줘야함
+    <Badge badgeContent={99} color='warning' sx={{ margin: '16px' }}>
+      {/* 테두리 */}
+      <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
+          border: '1px solid rgba(60, 57, 57, 0.5)',
+          borderRadius: '4px',
+          width: '300px',
+          minHeight: '110px',
+          backgroundColor: 'white',
+          padding: '4px',
         }}
       >
-        <Box
+        {/* 내용 */}
+        <Stack
+          spacing={1}
           sx={{
-            // minHeight: '28px',
             display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            position: 'relative',
+            flexDirection: 'column',
           }}
         >
           <Box
             sx={{
+              // minHeight: '28px',
               display: 'flex',
               flexDirection: 'row',
               justifyContent: 'center',
               alignItems: 'center',
+              position: 'relative',
             }}
           >
             <Box
-              component='img'
-              src={gameImg[chatRoomInfo.game]}
-              alt='game_img'
               sx={{
-                maxHeight: '24px',
-                maxWidth: '32px',
-                marginRight: '5px',
-              }}
-            />
-            <Typography sx={{ ml: 1, fontSize: '14px' }}>
-              <strong>{chatRoomInfo.createdBy}</strong>님의 파티
-            </Typography>
-            <Typography sx={{ ml: 2, color: '#5383e8', fontWeight: 'bold' }}>
-              {`[${chatRoomInfo.memberList.length} / ${chatRoomInfo.maxMember}]`}
-            </Typography>
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            fontSize: '14px',
-            textAlign: 'center',
-            wordBreak: 'break-all',
-          }}
-        >
-          {chatRoomInfo.content}
-        </Box>
-        <Box
-          sx={{
-            height: '30px',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-around',
-          }}
-        >
-          {/* 게시글 상세보기로 이동 */}
-          <Link
-            to={`${chatRoomInfo.roomId}`}
-            state={{ background: location }}
-            style={{
-              textDecoration: 'none',
-              background: 'fixed',
-              width: '60%',
-              height: '80%',
-            }}
-          >
-            <Button
-              variant='outlined'
-              size='small'
-              sx={{
-                width: '100%',
-                marginBottom: '10px',
-                color: '#3c3939',
-                borderColor: '#3c3939',
-                '&:hover': {
-                  border: 'none',
-                  backgroundColor: 'rgba(60, 57, 57, 0.5)',
-                  color: 'white',
-                },
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
-              상세보기
-            </Button>
-          </Link>
-        </Box>
-      </Stack>
-    </Box>
+              <Box
+                component='img'
+                src={gameImg[chatRoomInfo.game]}
+                alt='game_img'
+                sx={{
+                  maxHeight: '24px',
+                  maxWidth: '32px',
+                  marginRight: '5px',
+                }}
+              />
+              <Typography sx={{ ml: 1, fontSize: '14px' }}>
+                <strong>{chatRoomInfo.createdBy}</strong>님의 파티
+              </Typography>
+              <Typography sx={{ ml: 2, color: '#5383e8', fontWeight: 'bold' }}>
+                {`[${chatRoomInfo.memberList.length} / ${chatRoomInfo.maxMember}]`}
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              fontSize: '14px',
+              textAlign: 'center',
+              wordBreak: 'break-all',
+              overflow: 'hidden',
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+            }}
+          >
+            {chatRoomInfo.content}
+          </Box>
+          <Box
+            sx={{
+              height: '30px',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            {/* 게시글 상세보기로 이동 */}
+            <Link
+              to={`${chatRoomInfo.roomId}`}
+              state={{ background: location }}
+              style={{
+                textDecoration: 'none',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                width: '90%',
+              }}
+            >
+              <Box>
+                <Button
+                  variant='outlined'
+                  size='small'
+                  sx={{
+                    width: '100%',
+                    marginBottom: '10px',
+                    color: '#3c3939',
+                  }}
+                >
+                  상세보기로 이동
+                </Button>
+              </Box>
+            </Link>
+          </Box>
+        </Stack>
+      </Box>
+    </Badge>
   );
 };
 
