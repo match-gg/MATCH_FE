03d5a326bd0eb2ce7e24f926175cf96208c91d24
Minggurazi <jhsong1450@naver.com>
2023-06-30 01:31:42 +0900

    feat : 로그아웃 시 notification message 지우는 동작 추가

--- src/components/main/MainHeader.js
+++ src/components/main/MainHeader.js
@@ -430,6 +430,7 @@ const MainHeader = ({ game }) => {
                     handleClose();
                     // request to /logout endpoint
                     logoutHandler();
+                    dispatch(notificationActions.REMOVE_ALL_MESSAGES);
                   }}
                 >
                   <ListItemIcon>
