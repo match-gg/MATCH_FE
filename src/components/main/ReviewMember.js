e2ed962d741a3c940c307b5b594a441edcc2e5cd
Sugeun <songsurl@naver.com>
2023-06-29 17:11:29 +0900

    feat : 리뷰 멤버 넓이 지정

--- src/components/main/ReviewMember.js
+++ src/components/main/ReviewMember.js
@@ -9,26 +9,30 @@ const ReviewMember = props => {
   
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', pt: 1 }}>
-      <Typography component='h2' sx={{ fontSize: 12, fontWeight: 700 }}>
-        {name} 님 평가하기
-      </Typography>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', pl: 2, pr: 1 }}>
-        <IconButton size='medium'>
-          <ThumbUpOffAltIcon sx={{ width: 24, height: 24 }} />
-        </IconButton>
-        <Typography sx={{ fontSize: 10, fontWeight: 600 }}>좋아요</Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 200 }}>
+        <Typography component='h2' sx={{ fontSize: 12, fontWeight: 700 }}>
+          {name} 님 평가하기
+        </Typography>
       </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', pr: 1 }}>
-        <IconButton size='medium'>
-          <ThumbDownOffAltIcon sx={{ width: 24, height: 24 }} />
-        </IconButton>
-        <Typography sx={{ fontSize: 10, fontWeight: 600 }}>싫어요</Typography>
-      </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <IconButton size='medium'>
-          <ReportGmailerrorredIcon sx={{ width: 24, height: 24 }} />
-        </IconButton>
-        <Typography sx={{ fontSize: 10, fontWeight: 600 }}>신고하기</Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 200 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', pr: 2 }}>
+          <IconButton size='medium'>
+            <ThumbUpOffAltIcon sx={{ width: 24, height: 24 }} />
+          </IconButton>
+          <Typography sx={{ fontSize: 10, fontWeight: 600 }}>좋아요</Typography>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', pr: 2 }}>
+          <IconButton size='medium'>
+            <ThumbDownOffAltIcon sx={{ width: 24, height: 24 }} />
+          </IconButton>
+          <Typography sx={{ fontSize: 10, fontWeight: 600 }}>싫어요</Typography>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <IconButton size='medium'>
+            <ReportGmailerrorredIcon sx={{ width: 24, height: 24 }} />
+          </IconButton>
+          <Typography sx={{ fontSize: 10, fontWeight: 600 }}>신고하기</Typography>
+        </Box>
       </Box>
     </Box>
   );
